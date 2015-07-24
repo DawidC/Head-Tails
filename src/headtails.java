e46ac11f7a73af4a0124b2a05afab20daac3410e
DawidC <dawid.code+github@gmail.com>
2015-07-24 16:03:29 +0200

    zrobione menu

--- src/headtails.java
+++ src/headtails.java
@@ -3,36 +3,41 @@
  */
 import java.util.Scanner;
 public class headtails {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Gra orzeł - reszka");
         Scanner sc = new Scanner(System.in);
 
-        label: do{
-            System.out.println("Co wybierasz? 1.orzeł, 2.reszka");
+        System.out.println("Co wybierasz? 1.orzeł, 2.reszka");
 
-            System.out.print(": ");
-            int menu = sc.nextByte();
-            if (menu == 1) {
+        System.out.print(": ");
+        int menu = sc.nextByte();
+        int x = 0;
+        while(x==0){
+            switch (menu) {
+                case 1:
+                    System.out.println("Wybrałeś orła!");
+                    System.out.print("Czy chcesz grać dalej? t/n");
+                    String wybor = sc.next();
+                    char wyb = wybor.charAt(0);
+                    if(wyb == 'n'){
+                        x = 1;
+                    } else {
+                        x = 0;
+                    }
+                    break;
 
-                System.out.print("Jeszcze raz?(t/n): ");
-                String m = sc.next();
-                if(m == " "){
-                    break label;
-                } else {
-                    System.exit(1);
-                }
-            } else if (menu == 2) {
-                System.out.print("Jeszcze raz?(t/n): ");
-                String m = sc.next();
-                if(m == " "){
-                    break label;
-                } else {
-                    System.exit(2);
-                }
-            } else {
-                System.out.println("Zły wybór! Spróbuj jeszcze raz. ");
-                break label;
+                case 2:
+                    System.out.println("Wybrałeś reszkę!");
+                    System.out.print("Czy chcesz grać dalej? t/n");
+                    String wybor2 = sc.next();
+                    char wyb2 = wybor2.charAt(0);
+                    if(wyb2 == 'n'){
+                        x = 1;
+                    } else {
+                        x = 0;
+                    }
+                    break;
             }
-        } while(true);
+        }
     }
 }
